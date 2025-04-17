0208a41cff75540ff388f11d4f70158f1b711d1c
Penk0vXd <stanimir.penkov19@gmail.com>
2025-04-17 10:04:45 +0300

    Добавяне на всички задачи от теста

---
+++ test/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int max = nums.Min();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > max)
+                {
+                    max = nums[i];
+                }
+            }
+            Console.WriteLine($"Най - голямия елемент е: {max}");
+
+        }
+    }
+}
---
+++ test/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int broj = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 9)
+                {
+                    broj++;
+                }
+            }
+            Console.WriteLine($"Числото 9 се среща {broj} пъти");
+        }
+    }
+}
