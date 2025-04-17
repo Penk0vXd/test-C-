86c3f94bf575325612e5b5df41e3a1b0d2883548
Penk0vXd <stanimir.penkov19@gmail.com>
2025-04-17 10:43:03 +0300

    add file

---
+++ C# test/zad-1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace zad_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int sum = 0;
+            int max = int.MinValue;
+            int broj = 0;
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                sum += num[i];
+
+                if (num[i] > max)
+                {
+                    max = num[i];
+                }
+
+                if (num[i] == 9)
+                {
+                    broj++;
+                }
+            }
+            Console.WriteLine($"Сумата на елементите: {sum}");
+            Console.WriteLine($"Най - голямия елемент е: {max}");
+            Console.WriteLine($"Числото 9 се среща {broj} пъти");
+        }
+    }
+}
